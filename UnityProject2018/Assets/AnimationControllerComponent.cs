42b968613645b65b17006ed8e5ceeda78c359291
Dante Cerron <dacerron@gmail.com>
2021-05-12 11:04:26 -0700

    defined animationDescription SO and stubbed animationControllerComponent

---
+++ UnityProject2018/Assets/AnimationControllerComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerComponent : MonoBehaviour
+{
+    public List<AnimationDescription> animation;
+
+    private float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Inside Coroutine:
+        //      read next animation from list
+        //      fetch corresponding gameobject
+        //      set corresponding trigger on gameobject
+        //      wait an amount of time set by either technician or animation clip length
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Define Coroutine that lasts as long as an animation (or decide what the interval should be)
+}
