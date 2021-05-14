c45372a3911cfcc4a23780b049f6219c1cafb4f1
nkoda <dumriquenikko@gmail.com>
2021-05-14 01:15:22 -0700

    Communication between SO and object animators with animator controller is working. TODO: implement the animation trigger and refactor

--- UnityProject2018/Assets/AnimationControllerComponent.cs
+++ UnityProject2018/Assets/AnimationControllerComponent.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AnimationControllerComponent : MonoBehaviour
 {
-    public List<AnimationDescription> animation;
+    public List<AnimationDescription> animations;
 
-    private float waitTime;
+    private float waitTime = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("AnimationRoutine");
         // Inside Coroutine:
         //      read next animation from list
         //      fetch corresponding gameobject
@@ -17,6 +19,31 @@ public class AnimationControllerComponent : MonoBehaviour
         //      wait an amount of time set by either technician or animation clip length
     }
 
+    //Coroutine
+    IEnumerator AnimationRoutine()
+    {
+        List<string> animatedObjects;
+        List<string> triggerToSet;
+      
+        foreach(AnimationDescription animation in animations)
+        {
+            animatedObjects = animation.AnimatedObjects;
+            triggerToSet = animation.TriggerToSet;
+            var dic = animatedObjects.Zip(triggerToSet, (k, v) => new { k, v })
+                .ToDictionary(x => x.k, x => x.v);
+
+            foreach(KeyValuePair<string, string> gameObjectTrigger in dic)
+            {
+                Debug.Log(string.Join(", ", gameObjectTrigger.Key));
+                Debug.Log(string.Join(", ", gameObjectTrigger.Value));
+                Debug.Log("-------------------");
+                GameObject gameObject = GameObject.Find(gameObjectTrigger.Key);
+                (gameObject.GetComponent<Animator>()).SetTrigger(gameObjectTrigger.Key);
+            }
+       
+        }
+        yield return new WaitForSeconds(waitTime);
+    }
     // Update is called once per frame
     void Update()
     {
