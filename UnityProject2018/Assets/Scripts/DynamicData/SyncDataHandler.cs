fdad3e43cbe38c644f1a334953b703fccdf7db7e
nkoda <dumriquenikko@gmail.com>
2021-06-25 00:41:03 -0700

    Data syncing fixed
    Data syncing fixed. TODO change SO parameters to update once database is set up

--- UnityProject2018/Assets/Scripts/DynamicData/SyncDataHandler.cs
+++ UnityProject2018/Assets/Scripts/DynamicData/SyncDataHandler.cs
@@ -1,49 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using System.IO;
 using System.Xml;
 
 public class SyncDataHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
     public static string path = "Assets/Resources/Data/query.xml";
+    public static string fileDestination = "Assets/Resources/Data/query.xml";
     public List<PathwaySO> pathways;
+    public QueriesSO queries;
+
+    // private XmlDocument 
     void Start()
     {
-        
-        LoadXML();
+        UpdatePathways();
     }
 
-    void QueryData()
+    void UpdatePathways() 
+    {
+        foreach(PathwaySO pathway in pathways)
+        {
+            UpdateNodes(pathway.nodes);
+            UpdateEdges(pathway.edges);
+        }
+    }
+
+    void UpdateEdges(List<EdgeSO> edges) 
+    {
+        foreach (EdgeSO edge in edges)
+        {
+            //TODO change query according to database.
+            string query = QueryStatement(edge.QID, queries.testOnWikiData);
+            StartCoroutine(UpdateEdgeSO(edge, query));
+        }
+    }
+
+    void UpdateNodes(List<NodeSO> nodes) 
+    {
+        foreach(NodeSO node in nodes) 
+        {
+            //TODO change query according to database.
+            string query = QueryStatement(node.QID, queries.testOnWikiData);
+            StartCoroutine(UpdateNodeSO(node, query));
+        }
+    }
+
+    string QueryStatement(string qid, string query)
     {
         string WQS = "http://query.wikidata.org/sparql?query=";
-        string queryRaw = "SELECT ?item ?itemLabel " +
-            "WHERE {wd:Q50293158 wdt:P527 ?item. " +
-            "SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". " +
-            "} " +
-            "}";
-        string queryReady = UnityWebRequest.EscapeURL(queryRaw);
-        StartCoroutine(GetRequest(WQS + queryReady));
+        query = query.Replace("ID", qid);
+        string queryReady = UnityWebRequest.EscapeURL(query);
+        return WQS + queryReady;
     }
 
-    private void LoadXML()
+    IEnumerator UpdateNodeSO(NodeSO node, string uri)
     {
-        /*
-        XmlDocument doc = new XmlDocument();
-        doc.Load(path);
-
-        //the name of the object
-        XmlNodeList elem = doc.GetElementsByTagName("literal");
-        List<string> node_names = new List<string>();
-        foreach (XmlNode node in elem)
-            node_names.Add(node.InnerText);
-
-        for (int i = 0; i < nodes.Count; i++)
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
-            nodes[i].Label = node_names[i];
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(webRequest.downloadHandler.text);
+                XmlNodeList queriedData = doc.GetElementsByTagName("result")[0].ChildNodes;
+                //TODO update parameters that we want to update
+                node.Label = queriedData[0].InnerText;
+                node.Description = queriedData[1].InnerText;
+            }
         }
-        */
+    }
 
+    IEnumerator UpdateEdgeSO(EdgeSO edge, string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(webRequest.downloadHandler.text);
+                XmlNodeList queriedData = doc.GetElementsByTagName("result")[0].ChildNodes;
+                //TODO update parameters that we want to update
+                edge.Label = queriedData[0].InnerText;
+            }
+        }
+    }
+
+    //function to help debug
+    static void SaveXML(string str) {
+        string fileDestination = "Assets/Resources/Data/query.xml";
+        FileStream filestream = new FileStream(fileDestination, FileMode.Create);
+        StreamWriter writer = new StreamWriter(filestream);
+        str = str.Replace("http://www.wikidata.org/entity/Q","");
+        writer.Write(str);
+        writer.Close();
     }
 }
+
--- UnityProject2018/Assets/Scripts/ScriptableObjects/DataTemplates/PathwaySO.cs
+++ UnityProject2018/Assets/Scripts/ScriptableObjects/DataTemplates/PathwaySO.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Pathway", menuName = "Pathway")]
 public class PathwaySO : ScriptableObject
 {
+    public string QID;
     public List<NodeSO> nodes;
     public List<EdgeSO> edges;
-
 }
