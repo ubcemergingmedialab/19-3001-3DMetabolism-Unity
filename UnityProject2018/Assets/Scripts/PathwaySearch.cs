7ed01e9a8f4c166bde39e58e4cb6cdb04d0b7295
Mohsen M <dev.movahedi@gmail.com>
2021-11-15 15:55:35 -0800

    photos uploaded

---
+++ UnityProject2018/Assets/Scripts/PathwaySearch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PathwaySearch : MonoBehaviour
+{
+
+    public GameObject SearchInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PrintPathwaySearch() {
+        List<PathwaySO> results = StatusController.Instance.activePathways;
+        string input = SearchInput.GetComponent<TMP_InputField>().text;
+        foreach (PathwaySO pw in results){
+
+            if (pw.Label == input){
+                Debug.Log(""+ pw.Label);
+                break;
+            } else{
+                continue;
+            }
+        }
+    }
+}
