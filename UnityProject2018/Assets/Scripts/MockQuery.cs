a67890bb85918e64e57b7c375d4e25580d7bbf17
Karen <zhoyizhen_1998@hotmail.com>
2022-01-14 16:55:42 -0800

    finished the bfs and tested. it works fam!

--- UnityProject2018/Assets/Scripts/MockQuery.cs
+++ UnityProject2018/Assets/Scripts/MockQuery.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MockQuery : MonoBehaviour
 {
     public List<EdgeSOBeta> edges;
-    public List<NodeSOBeta> node;
+    public List<NodeSOBeta> nodes;
     // Start is called before the first frame update
 
     /* TODO:
@@ -19,12 +19,18 @@ public class MockQuery : MonoBehaviour
         glycogenSynthasePathway.LocalNetwork = new Dictionary<NodeSOBeta, List<EdgeSOBeta>>();
         // hexokinase , glucose -> glucose6phospahate 
         EdgeSOBeta hexokinase = ScriptableObject.CreateInstance<EdgeSOBeta>();
+        edges.Add(hexokinase);
         hexokinase.Label = "hexokinase";
+        hexokinase.name = "hexokinase";
 
         NodeSOBeta glucose = ScriptableObject.CreateInstance<NodeSOBeta>();
+        nodes.Add(glucose);
         glucose.Label = "glucose";
+        glucose.name = "glucose";
         NodeSOBeta glucose6phosphate = ScriptableObject.CreateInstance<NodeSOBeta>();
+        nodes.Add(glucose6phosphate);
         glucose6phosphate.Label = "glucose-6-phosphate";
+        glucose6phosphate.name = "glucose-6-phosphate";
 
         hexokinase.reactants = new List<NodeSOBeta>();
         hexokinase.reactants.Add(glucose);
@@ -35,11 +41,16 @@ public class MockQuery : MonoBehaviour
         glycogenSynthasePathway.LocalNetwork.Add(glucose6phosphate, new List<EdgeSOBeta>{hexokinase});
 
         // Phosphoglucose mutase , glucose 6-phosphate => glucose 1-phosphate
-        EdgeSOBeta phosphoglucoseMutase = ScriptableObject.CreateInstance<EdgeSOBeta>();
+        EdgeSOBeta phosphoglucoseMutase = ScriptableObject.CreateInstance<EdgeSOBeta>();        
+        edges.Add(phosphoglucoseMutase);
         phosphoglucoseMutase.Label = "phosphoglucose mutase";
+        phosphoglucoseMutase.name = "phosphoglucose mutase";
+
 
         NodeSOBeta glucose1phosphate = ScriptableObject.CreateInstance<NodeSOBeta>();
+        nodes.Add(glucose1phosphate);
         glucose1phosphate.Label = "glucose-1-phosphate";
+        glucose1phosphate.name = "glucose-1-phosphate";
 
         phosphoglucoseMutase.reactants = new List<NodeSOBeta>();
         phosphoglucoseMutase.reactants.Add(glucose6phosphate);
@@ -51,10 +62,14 @@ public class MockQuery : MonoBehaviour
 
         // UDP-glucose pyrophosphorylase , UTP + glucose 1-phosphate <=> UDP-glucose + PPi     --> bi directional!
         EdgeSOBeta UDPGlucosePyrophosphorylase = ScriptableObject.CreateInstance<EdgeSOBeta>();
+        edges.Add(UDPGlucosePyrophosphorylase);
         UDPGlucosePyrophosphorylase.Label = "UDP-glucose pyrophosphorylase";
+         UDPGlucosePyrophosphorylase.name = "UDP-glucose pyrophosphorylase";
 
         NodeSOBeta UDPglucose = ScriptableObject.CreateInstance<NodeSOBeta>();
+        nodes.Add(UDPglucose);
         UDPglucose.Label = "UDP-glucose";
+        UDPglucose.name = "UDP-glucose";
 
         UDPGlucosePyrophosphorylase.reactants = new List<NodeSOBeta>();
         UDPGlucosePyrophosphorylase.reactants.Add(glucose1phosphate);
@@ -68,12 +83,18 @@ public class MockQuery : MonoBehaviour
 
         // glycogen synthase, glycogen (n residues) + UDP-glucose => UDP + glycogen (n+1 residues)
         EdgeSOBeta glycogenSynthase = ScriptableObject.CreateInstance<EdgeSOBeta>();
+        edges.Add(glycogenSynthase);
         glycogenSynthase.Label = "glycogenSynthase";
+        glycogenSynthase.name = "glycogenSynthase";
 
         NodeSOBeta glycogen_n1 = ScriptableObject.CreateInstance<NodeSOBeta>();
+        nodes.Add(glycogen_n1);
         glycogen_n1.Label = "glycogen(n+1)";
+        glycogen_n1.name = "glycogen(n+1)";
         NodeSOBeta glycogen_n = ScriptableObject.CreateInstance<NodeSOBeta>();
+        nodes.Add(glycogen_n);
         glycogen_n.Label = "glycogen(n)";
+        glycogen_n.name = "glycogen(n)";
 
 
         glycogenSynthase.reactants = new List<NodeSOBeta>();
@@ -87,7 +108,9 @@ public class MockQuery : MonoBehaviour
         glycogenSynthasePathway.LocalNetwork[UDPglucose].Add(glycogenSynthase);
         // glycogen phosphorylase, glycogen (n+1 residues) + Pi => glycogen (n residues) + glucose 1-phosphate  --> not in wikibase!, needs to be checked with a theory
         EdgeSOBeta glycogenPhosphorylase = ScriptableObject.CreateInstance<EdgeSOBeta>();
+        edges.Add(glycogenPhosphorylase);
         glycogenPhosphorylase.Label = "glycogen Phosphorylase";
+        glycogenPhosphorylase.name = "glycogen Phosphorylase";
 
         glycogenPhosphorylase.reactants = new List<NodeSOBeta>();
         glycogenPhosphorylase.reactants.Add(glycogen_n1);
@@ -98,19 +121,24 @@ public class MockQuery : MonoBehaviour
         glycogenSynthasePathway.LocalNetwork[glycogen_n1].Add(glycogenPhosphorylase);
         glycogenSynthasePathway.LocalNetwork[glucose1phosphate].Add(glycogenPhosphorylase);        
         glycogenSynthasePathway.LocalNetwork[glycogen_n].Add(glycogenPhosphorylase);
+
+
+
+
+        BFSTest(glycogenSynthasePathway,glucose6phosphate,glycogen_n);
     }
 
-      list<ScriptableObject> SearchForPath(PathwaySOBeta pathway, NodeSOBeta nodeRoot, NodeSOBeta nodeToFind) {
+      List<ScriptableObject> SearchForPath(PathwaySOBeta pathway, NodeSOBeta nodeRoot, NodeSOBeta nodeToFind) {
 
        Queue<List<ScriptableObject>> BFSQueue = new Queue<List<ScriptableObject>>();
        Dictionary<string,bool> visited = new Dictionary<string, bool>();
 
-       BFSQueue.Enqueue(new list<ScriptableObject>(){nodeRoot});
+       BFSQueue.Enqueue(new List<ScriptableObject>(){nodeRoot});
        visited.Add(nodeRoot.Label,true);
 
        while (BFSQueue.Count > 0) {
            List<ScriptableObject> currentPath = BFSQueue.Dequeue();
-           NodeSOBeta currentNode = currentPath[currentPath.Count - 1];
+           NodeSOBeta currentNode = (NodeSOBeta) currentPath[currentPath.Count - 1];
            if (currentNode.Label == nodeToFind.Label){
                // print the path later
                Debug.Log("found the node : " + currentNode.Label + " = " + nodeToFind.Label);
@@ -127,17 +155,24 @@ public class MockQuery : MonoBehaviour
             // BFSQueue.Enqueue(currentPath)
 
             // in case this doesnt duplicate, create a new list per iteration and enqueue .
-            // new list of SO =of current path, enqueu this instead
+            // new list of SO =of current path, enqueue this instead
 
-            List<ScriptableObject> newPath = new List<ScriptableObject>(currentPath);
+            
             foreach(KeyValuePair <EdgeSOBeta, List<NodeSOBeta>> entry in children) {
-                newPath.Add(entry.Key);
-                newPath.AddRange(entry.Value);
+                
 
-                if (visited.ContainsKey(newPath[newPath.Count - 1])){
+                foreach (NodeSOBeta curr in entry.Value){
+                    List<ScriptableObject> newPath = new List<ScriptableObject>(currentPath);
+                    newPath.Add(entry.Key);
+                    newPath.Add(curr);
+                    //string lastNodeLabel = newPath[newPath.Count - 1].Label;
+                    if (visited.ContainsKey(curr.Label)){
                     continue;
+                    }
+                    visited.Add(curr.Label,true);
+                    BFSQueue.Enqueue(newPath);
                 }
-                BFSQueue.Enqueue(newPath);
+                
             }
             
            
@@ -168,19 +203,17 @@ public class MockQuery : MonoBehaviour
                 if (currentEdge.products.Contains(current)){
 
                     foreach (NodeSOBeta node in currentEdge.reactants){
-                        if(!visited.ContainsKey(node.Label)){
-                            nodesByEdge[currentEdge].Add(node);
-                            visited.Add(node.Label,true);
-                        }
+                        nodesByEdge[currentEdge].Add(node);
+
                     }
                 } else {
                     if (currentEdge.reactants.Contains(current)){
 
                         foreach (NodeSOBeta node in currentEdge.products){
-                            if(!visited.ContainsKey(node.Label)){
+                            // if(!visited.ContainsKey(node.Label)){
                                 nodesByEdge[currentEdge].Add(node);
-                                visited.Add(node.Label,true);
-                            }
+                        //         visited.Add(node.Label,true);
+                        //     }
                         }
                     }
                 }
@@ -188,10 +221,10 @@ public class MockQuery : MonoBehaviour
                 if (currentEdge.reactants.Contains(current)){
 
                     foreach (NodeSOBeta node in currentEdge.products){
-                        if(!visited.ContainsKey(node.Label)){
+                        // if(!visited.ContainsKey(node.Label)){
                             nodesByEdge[currentEdge].Add(node);
-                            visited.Add(node.Label,true);
-                        }
+                        //     visited.Add(node.Label,true);
+                        // }
                     }
                 }
             }
@@ -200,6 +233,17 @@ public class MockQuery : MonoBehaviour
     }
 
 
+
+    public void BFSTest(PathwaySOBeta pathway, NodeSOBeta start, NodeSOBeta end) {
+        List<ScriptableObject> result =  SearchForPath(pathway,start,end);
+        string printResult = "search in " + pathway.name + "from node:" + start.Label +  " - end node:" + end.Label;
+        int n = 1;
+        foreach (ScriptableObject step in result){
+            printResult += "\n" + n + " - " + step.name; //GetType().ToString();
+            n++;   
+        }
+        Debug.Log(printResult);
+    }
 
 
 
