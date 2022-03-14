a5531b7a122e33aad48623e44d505afda92ced07
dacerron <dacerron@gmail.com>
2022-03-14 10:24:33 -0700

    UnityProject2018/Assets/Resources/Data/query.xml

--- UnityProject2018/Assets/Scripts/WikiDataConnect.cs
+++ UnityProject2018/Assets/Scripts/WikiDataConnect.cs
@@ -11,13 +11,29 @@ public class WikiDataConnect : MonoBehaviour
     public static string fileDestination = "Assets/Resources/Data/query.xml";
     void Start()
     {
-        string WQS = "http://query.wikidata.org/sparql?query=";
-        string queryRaw = "SELECT ?item ?itemLabel " +
-            "WHERE {wd:Q50293158 wdt:P527 ?item. " +
-            "SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". " +
-            "} " +
-            "}";
+        string WQS = "http://wikibase-3dm.eml.ubc.ca:8282/proxy/wdqs/bigdata/namespace/wdq/sparql?format=json&query=";
+        string queryRaw = "PREFIX foaf: <http://wikibase-3dm.eml.ubc.ca/entity/>" +
+"select distinct ?prefixedEdge" +
+"(strafter(?prefixedEdge,\":\") as ?edgeQID)" +
+"?prefixedMetabolite" +
+"(strafter(?prefixedMetabolite,\":\") as ?metaboliteQID)" +
+"?edgeLabel ?metaboliteLabel ?isReactant ?isProduct" +
+"?prefixedEnzyme" +
+"(strafter(?prefixedEnzyme,\":\") as ?enzymeQID) ?enzymeLabel where {" +
+"foaf:Q88 wdt:P4 ?edge." +
+"?edge p:P4 ?statement." +
+"?edge wdt:P14 ?enzyme." +
+"?statement ps:P4 ?metabolite." +
+"?statement pq:P31|pq:P32 ?edge." +
+"BIND(replace(str(?edge), str(foaf:), \"foaf:\") as ?prefixedEdge)" +
+"BIND(replace(str(?metabolite), str(foaf:), \"foaf:\") as ?prefixedMetabolite)" +
+"BIND(replace(str(?enzyme), str(foaf:), \"foaf:\") as ?prefixedEnzyme)" +
+"BIND ( EXISTS { ?statement pq:P31 ?edge } as ?isReactant )" +
+"BIND ( EXISTS { ?statement pq:P32 ?edge } as ?isProduct )" +
+"SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". }" +
+"}";
         string queryReady = UnityWebRequest.EscapeURL(queryRaw);
+        Debug.Log(WQS +queryReady);
         StartCoroutine(GetRequest(WQS + queryReady));
     }
 
@@ -26,6 +42,7 @@ public class WikiDataConnect : MonoBehaviour
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
             // Request and wait for the desired page.
+            webRequest.SetRequestHeader("content-type", "application/sparql-results+json");
             yield return webRequest.SendWebRequest();
             string[] pages = uri.Split('/');
             int page = pages.Length - 1;
@@ -35,6 +52,7 @@ public class WikiDataConnect : MonoBehaviour
             }
             else
             {
+                Debug.Log(webRequest.downloadHandler.text);
                 WriteString(webRequest.downloadHandler.text);
             }
         }
