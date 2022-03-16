39bbdebbf82190c4c858f28c3ba000217b6e21b6
dacerron <dacerron@gmail.com>
2022-03-16 10:42:55 -0700

    added classes for interpreting wikibase results from fetch

--- UnityProject2018/Assets/Scripts/QueryServices/QuerySOInitializer.cs
+++ UnityProject2018/Assets/Scripts/QueryServices/QuerySOInitializer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
-
+using UnityEngine.Networking;
 
 
 public class QuerySOInitializer : MonoBehaviour 
@@ -23,17 +23,32 @@ string ResourceFolderPath = "Assets/Resources/Data/TestQuerySO/";
     StreamReader reader = new StreamReader(path);
     string jsonString = reader.ReadToEnd();
     reader.Close();
-    Debug.Log("<json>" + jsonString);
+    //Debug.Log("<json>" + jsonString);
 
-    QueryResults results = JsonUtility.FromJson<QueryResults>(jsonString);
-
-    Debug.Log("<json>" + results.QueryResultList.Count);
-
-    foreach( QueryItem item in results.QueryResultList){
-        Debug.Log("<json>" + item.metaboliteLabel);
-        NodeSOInit(item);
-
-    }
+    string WQS = "http://wikibase-3dm.eml.ubc.ca:8282/proxy/wdqs/bigdata/namespace/wdq/sparql?format=json&query=";
+        string queryRaw = "PREFIX foaf: <http://wikibase-3dm.eml.ubc.ca/entity/>" +
+"select distinct ?prefixedEdge" +
+"(strafter(?prefixedEdge,\":\") as ?edgeQID)" +
+"?prefixedMetabolite" +
+"(strafter(?prefixedMetabolite,\":\") as ?metaboliteQID)" +
+"?edgeLabel ?metaboliteLabel ?isReactant ?isProduct" +
+"?prefixedEnzyme" +
+"(strafter(?prefixedEnzyme,\":\") as ?enzymeQID) ?enzymeLabel where {" +
+"foaf:Q88 wdt:P4 ?edge." +
+"?edge p:P4 ?statement." +
+"?edge wdt:P14 ?enzyme." +
+"?statement ps:P4 ?metabolite." +
+"?statement pq:P31|pq:P32 ?edge." +
+"BIND(replace(str(?edge), str(foaf:), \"foaf:\") as ?prefixedEdge)" +
+"BIND(replace(str(?metabolite), str(foaf:), \"foaf:\") as ?prefixedMetabolite)" +
+"BIND(replace(str(?enzyme), str(foaf:), \"foaf:\") as ?prefixedEnzyme)" +
+"BIND ( EXISTS { ?statement pq:P31 ?edge } as ?isReactant )" +
+"BIND ( EXISTS { ?statement pq:P32 ?edge } as ?isProduct )" +
+"SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". }" +
+"}";
+        string queryReady = UnityWebRequest.EscapeURL(queryRaw);
+        //Debug.Log(WQS +queryReady);
+        StartCoroutine(GetRequest(WQS + queryReady));
     
 }
 
@@ -78,6 +93,35 @@ public void NodeSOInit(QueryItem item){
         }
     }
 }
+
+IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            webRequest.SetRequestHeader("content-type", "application/sparql-results+json");
+            yield return webRequest.SendWebRequest();
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+                WikibaseResult result = JsonUtility.FromJson<WikibaseResult>(webRequest.downloadHandler.text);
+
+            Debug.Log("<json>" + result.results.bindings.Count);
+
+            foreach( WikibaseBinding item in result.results.bindings){
+                Debug.Log("<json>" + item.metaboliteLabel.value);
+                NodeSOInit(item.metaboliteLabel.value);
+
+            }
+            }
+        }
+    }
 
 
 }
---
+++ UnityProject2018/Assets/Scripts/QueryServices/WikibaseBinding.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WikibaseBinding
+{
+    public WikibaseBindingElement prefixedEdge;
+    public WikibaseBindingElement prefixedMetabolite;
+    public WikibaseBindingElement prefixedEnzyme;
+    public WikibaseBindingElement edgeLabel;
+    public WikibaseBindingElement metaboliteLabel;
+    public WikibaseBindingElement enzymeLabel;
+    public WikibaseBindingElement isReactant;
+    public WikibaseBindingElement isProduct;
+    public WikibaseBindingElement edgeQID;
+    public WikibaseBindingElement metaboliteQID;
+    public WikibaseBindingElement enzymeQID;
+
+    public WikibaseBindingElement isBidirectional;
+}
