dfeed5fe7db3df9f33ba7c280a106c9d02139a2f
Tom Mo <tmo217@protonmail.com>
2022-11-07 14:57:53 -0800

    Comment format + GetMathingNodes

--- UnityProject2018/Assets/Scripts/ScriptableObjects/DataTemplates/ConnectionsSO.cs
+++ UnityProject2018/Assets/Scripts/ScriptableObjects/DataTemplates/ConnectionsSO.cs
@@ -1,36 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+/// <summary>
+/// General class for node-edge connections, extended by PathwaySO. 
+/// </summary>
 [CreateAssetMenu(fileName = "New Connections", menuName = "Connections")]
-
-/***
-* General class for node-edge connections, extended by PathwaySO
-*  - Stores edges of a node as an adjacency matrix
-*/
 public class ConnectionsSO : ScriptableObject
 {
+    /// <summary>
+    /// Stores edges of a node as an adjacency matrix
+    /// </summary>
     public Dictionary<NodeSO, HashSet<EdgeSO>> LocalNetwork; 
 
     public ConnectionsSO(){
         LocalNetwork = new Dictionary<NodeSO, HashSet<EdgeSO>>() ;
     }
 
-    // Add node to the lcoal network dictionary
+    /// <summary>
+    /// Add node to the lcoal network dictionary
+    /// </summary>
+    /// <param name="node"> Node to be added </param>
     public void AddNode(NodeSO node) {
         if (!(LocalNetwork.ContainsKey(node))){
             LocalNetwork.Add(node, new HashSet<EdgeSO>());
         }
     }
 
-    // add an edge to a node inside the Local network dictionary,
-    // skip if already added
+    /// <summary>
+    /// Add an edge to a node inside the Local network dictionary, 
+    /// skip if already added
+    /// </summary>
+    /// <param name="parentNode"> parent node to the edge </param>
+    /// <param name="edge"> edge connected to parent node </param>
     public void AddEdge(NodeSO parentNode, EdgeSO edge){
         LocalNetwork[parentNode].Add(edge);
     }
 
-    // Return all edges connected to a node in List<T> Type
-    public List<EdgeSO> GetEdgesAsList(NodeSO node)
+    /// <summary>
+    /// Return all edges connected to a node in List<T> Type
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    public List<EdgeSO> GetConnectedEdges(NodeSO node)
     {
         List<EdgeSO> list = new List<EdgeSO>();
         if (LocalNetwork.ContainsKey(node))
@@ -42,6 +55,23 @@ public class ConnectionsSO : ScriptableObject
             }
         }
         return list;
+    }
+
+    /// <summary>
+    /// Search for a collection of node labels based on keyword
+    /// </summary>
+    /// <param name="keyword"> Keyword pattern to match </param>
+    /// <returns> List of node labels </returns>
+    public List<string> GetMatchingNodes(string keyword)
+    {
+        IEnumerable<NodeSO> query = LocalNetwork.Keys.Where<NodeSO>(node => node.Label == "something"); // TODO: change to regex
+
+        List<string> matchedNodes = new List<string>(); 
+        foreach (NodeSO node in query)
+        {
+            matchedNodes.Add(node.Label);
+        }
+        return new List<string>();
     }
 
     public IDictionaryEnumerator GetLocalNetworkEnumerator()
