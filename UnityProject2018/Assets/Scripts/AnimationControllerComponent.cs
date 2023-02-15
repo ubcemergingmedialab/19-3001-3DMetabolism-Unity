600b05c0cc6a928cecc9c77a89a7be865f519d21
Shum <robert.shum@ubc.ca>
2023-02-15 11:42:28 -0800

    Animation Updates from IAT4U and Enabled Sidecards for Edge/Pathways
    Renamed v0.0.0 to IAT4U 3DM Example Scene.  What the heck does the original name even mean.
    Fixed timer for AnimateMaterial so it works (was lerping incorrectly).
    Enabled sidecards to show (edges + pathways).
    Expanded on the example for IAT4U by using one pathway as an example.
    Code commenting.

--- UnityProject2018/Assets/Scripts/AnimationControllerComponent.cs
+++ UnityProject2018/Assets/Scripts/AnimationControllerComponent.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/// <summary>
+/// 2023-2-15
+/// Given a list of AnimationDescriptions, it will loop through each AD, and play the animation.  For example:
+/// if a AnimationDescription has an Animated Object of 'glucose-6-phosphate' and the trigger is 'GreyToWhite',
+/// it will look for that node of 'glucose-6-phosphate' and trigger 'GreyToWhite'.
+/// - Also does color highlighting of the materials AnimationDescriptionPresenter is present.
+/// </summary>
 public class AnimationControllerComponent : MonoBehaviour
 {
     public List<AnimationDescription> animations;
@@ -13,22 +20,34 @@ public class AnimationControllerComponent : MonoBehaviour
 
     private Coroutine animationRoutine;
 
+    /// <summary>
+    ///Assigns Presenter (optional)
+    ///Manually start coroutine here for testing
+    /// </summary>
     void Start()
     {
         //animationRoutine = StartCoroutine("PlayAnimations");
-        if(presenter == null) {
+        if (presenter == null)
+        {
             presenter = FindObjectOfType<AnimationDescriptionSliderPresenter>();
         }
+
+        //For testing
+        //StartCoroutine(PlayAnimations());
     }
 
+    /// <summary>
+    /// Goes through each AD, looks for it in the scene, and triggers it.  It also plays reset animation if presented.
+    /// When finished, it will restart from the first animation.
+    /// </summary>
     private IEnumerator PlayAnimations()
     {
-        foreach(AnimationDescription animation in animations)
+        foreach (AnimationDescription animation in animations)
         {
             Dictionary<string, string> animationDefinition;
             animationDefinition = DefineAnimation(animation);
             AnimateGameObject(animationDefinition);
-            if(presenter != null)
+            if (presenter != null)
             {
                 presenter.HighlightStep(int.Parse(animation.name));
             }
@@ -39,7 +58,12 @@ public class AnimationControllerComponent : MonoBehaviour
         animationRoutine = StartCoroutine("PlayAnimations");
     }
 
-    private Dictionary<string, string> DefineAnimation(AnimationDescription animation) 
+    /// <summary>
+    /// Given a single animation description, it will return a key value pair of it's name and trigger
+    /// </summary>
+    /// <param name="animation">the Animation Description Scriptable Object
+    /// <returns> A dictionary<string, string> of all the AD element name and it's triggers </returns>
+    private Dictionary<string, string> DefineAnimation(AnimationDescription animation)
     {
         Dictionary<string, string> animationDefinition;
         List<string> objectsToAnimate;
@@ -51,39 +75,53 @@ public class AnimationControllerComponent : MonoBehaviour
         return animationDefinition;
     }
 
+    /// <summary>
+    /// Given a key value pair of a animation definition, it will look for the animator of the key,
+    /// and then play the trigger of the animation.  For ex: if key is 'glucose-6-phosphate' and is found,
+    /// it will attempt to play 'GreyToWhite' trigger (whatever the value is).
+    /// </summary>
+    /// <param name="animationDefinition">string key valye pair of the animation definition
     private void AnimateGameObject(Dictionary<string, string> animationDefinition)
     {
         Animator gameObjectAnimator;
         foreach (KeyValuePair<string, string> animationStep in animationDefinition)
         {
             GameObject curGO = GameObject.Find(animationStep.Key);
-            if(curGO != null)
+            if (curGO != null)
             {
                 gameObjectAnimator = curGO.GetComponent<Animator>();
-                if(gameObjectAnimator != null)
+                if (gameObjectAnimator != null)
                 {
                     Debug.Log(curGO.name + " " + animationStep.Value);
                     gameObjectAnimator.Play(animationStep.Value);
-                } else
+                }
+                else
                 {
                     Debug.Log("didnt find animator in " + curGO.name);
                 }
-            } else
+            }
+            else
             {
                 Debug.Log("gameobject not found");
             }
         }
     }
 
+    /// <summary>
+    /// Given a key value pair of a animation definition, it will look for the animator of the key,
+    /// and then play the trigger of the animation, which is coded to 'Idle'.  For ex: if key is 'glucose-6-phosphate' and is found,
+    /// it will attempt to play 'Idle' trigger.
+    /// </summary>
+    /// <param name="animationDefinition">string key valye pair of the animation definition
     private void ResetGameObject(Dictionary<string, string> animationDefinition)
     {
         foreach (KeyValuePair<string, string> animationStep in animationDefinition)
         {
             GameObject curGO = GameObject.Find(animationStep.Key);
-            if(curGO != null)
+            if (curGO != null)
             {
                 Animator gameObjectAnimator = curGO.GetComponent<Animator>();
-                if(gameObjectAnimator != null)
+                if (gameObjectAnimator != null)
                 {
                     gameObjectAnimator.Play("Idle");
                 }
@@ -99,13 +137,18 @@ public class AnimationControllerComponent : MonoBehaviour
         }*/
     }
 
+    /// <summary>
+    /// Plays reset animation (if applicable), then loads the next set of animations (ex: a different pathway),
+    /// pauses based on resetTime, then plays the new list of animations.
+    /// </summary>
+    /// <param name="newAnimation">List of AnimationDescription scriptable objects.
     public IEnumerator ChangeAnimation(List<AnimationDescription> newAnimation)
     {
         Dictionary<string, string> reset = DefineAnimation(resetAnimation);
         AnimateGameObject(reset);
         yield return new WaitForSeconds(resetTime);
         ResetGameObject(reset);
-        if(animationRoutine != null)
+        if (animationRoutine != null)
         {
             StopCoroutine(animationRoutine);
         }
